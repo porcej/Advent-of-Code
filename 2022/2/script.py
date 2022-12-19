18ebf13dc2f0557a608c2fe089c36d4031cf6ec3
Joe Porcelli <porcej@gmail.com>
2022-12-18 21:20:02 -0500

    Day 2 part 1

---
+++ 2022/2/script.py
@@ -0,0 +1,53 @@
+p1_total = 0
+p2_total = 0
+
+# rock = 1 	    A   X
+# paper = 2     B   Y
+# scissors = 3  C   Z
+win_points = 6
+draw_points = 3
+lose_points = 0
+
+			#   Lose, Draw, Win	
+_input_file = ["B X", "B Y", "B Z"]
+
+with open("input.txt", "r") as _input_file:
+	for line in _input_file:
+		p1 = ord(line[0]) - ord("A")
+		p2 = ord(line[2]) - ord("X")
+		print(p2)
+
+		# You win
+		if (p1 + 1) % 3 == p2:
+			print("You win")
+			p1_total += lose_points + p1 + 1
+			p2_total +=  win_points + p2 + 1
+
+		# Its a draw
+		elif p1 == p2:
+			print("Draw")
+			p1_total +=  draw_points + p1 + 1
+			p2_total +=  draw_points + p2 + 1
+
+		# Else your opponent wins
+		else:
+			print("Lose")
+			p1_total += win_points + p1 + 1
+			p2_total += lose_points + p2 + 1
+
+
+
+
+# with open("input.txt", "r") as _input_file:
+# 	for line in _input_file:
+# 		if line == "\n":
+# 			current_elf += 1
+# 			elves.append(0)
+# 		else:
+# 			elves[current_elf] += int(line)
+
+part_1 = p2_total
+print("Part 1: {}".format(part_1))
+
+part_2 = 2
+print("Part 2: {}".format(part_2))
