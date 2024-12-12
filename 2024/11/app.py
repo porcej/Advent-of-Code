b5e602125a866ba5e7a0d3bf40729a2e3f17b8dd
Joseph Porcelli <porcej@gmail.com>
2024-12-12 09:17:44 -0500

    Day 11 part 2

--- 2024/11/app.py
+++ 2024/11/app.py
@@ -4,8 +4,8 @@ def load_stones(file_path):
     try:
         with open(file_path, 'r') as file:
             line = file.read().strip()
-            # stones = [int(ch) if ch.isdigit() else -1 for ch in line.split(" ")]
-            stones = map(int, line.split(' '))
+            stones = [int(ch) if ch.isdigit() else -1 for ch in line.split(" ")]
+            # stones = map(int, line.split(' '))
         return stones
 
     except FileNotFoundError:
@@ -15,32 +15,33 @@ def load_stones(file_path):
     except Exception as e:
         print(f"An unexpected error occurred: {e}")
 
-def blink(stones):
-    new_stones = []
-    for sdx, stone in enumerate(stones):
-        stone_str = str(stone)
-        # Rules
-        if stone == 0:
-            new_stones.append(1)
-        elif len(stone_str) % 2 == 0:
-            midpoint = len(stone_str) // 2
-            new_stones.append(int(stone_str[:midpoint]))
-            new_stones.append(int(stone_str[midpoint:]))
-        else:
-            new_stones.append(stone * 2024)
 
-    return new_stones
+seen = {}
+def blink(stone, blinks, scale=2024):
+    if (stone, blinks) in seen:
+        return seen[(stone, blinks)]
+    new_stones = []
+    if blinks == 0:
+        ret = 1
+    elif stone == 0:
+        ret = blink(1, blinks-1)
+    elif len(str(stone))  % 2 == 0:
+        stone_str = str(stone)
+        midpoint = len(stone_str) // 2
+        left, right = int(stone_str[:midpoint]), int(stone_str[midpoint:])
+        ret = blink(left, blinks-1) + blink(right, blinks-1)
+    else:
+        ret = blink(stone*scale, blinks-1)
+    seen[(stone,blinks)] = ret
+    return ret
+
 
 def part_one(stones, blinks=25):
-    for bdx in range(blinks):
-        stones = blink(stones)
-    return len(stones)
+    return sum(blink(stone, blinks) for stone in stones)
 
 
-
-def part_two(stones):
-    pass
-
+def part_two(stones, blinks=75):
+    return sum(blink(stone, blinks) for stone in stones)
 
 if __name__ == "__main__":
     # file_path = "example.txt"
@@ -50,6 +51,9 @@ if __name__ == "__main__":
     stones = load_stones(file_path)
     num_stones = part_one(stones)
     print(f'After blinking 25 times there are {num_stones}.')
+    print(stones)
+
     num_stones = part_two(stones)
-    print(f'After blinking 25 times there are {num_stones}.')
+    print(f'After blinking 75 times there are {num_stones}.')
+
 
