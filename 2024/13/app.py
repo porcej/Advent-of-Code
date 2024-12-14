d1f0a30f24c1e74f018390f3b1a128790ee9ce00
Joseph Porcelli <porcej@gmail.com>
2024-12-13 19:53:47 -0500

    Completed Day 13 PArt 1

--- 2024/13/app.py
+++ 2024/13/app.py
@@ -8,9 +8,9 @@ def load_machines(file_path):
             for machine_data in file.read().split('\n\n'):
                 raw_machine_data = list(map(int, re.findall(r'(\d+)', machine_data)))
                 machine = {
-                    'button_a': {'x': raw_machine_data[0], 'y':raw_machine_data[1]},
-                    'button_b': {'x': raw_machine_data[2], 'y':raw_machine_data[3]},
-                    'prize': {'x': raw_machine_data[4], 'y':raw_machine_data[5]}
+                    'a': (raw_machine_data[0], raw_machine_data[1]),
+                    'b': (raw_machine_data[2], raw_machine_data[3]),
+                    'p': (raw_machine_data[4], raw_machine_data[5])
                 }
                 machines.append(machine)
         return machines
@@ -22,9 +22,20 @@ def load_machines(file_path):
     except Exception as e:
         print(f"An unexpected error occurred: {e}")
 
+def button_press(machine, cost_a=3, cost_b=1):
+    a,b,p = machine['a'], machine['b'], machine['p']
+    a_presses = (b[0]*p[1] - b[1]*p[0])/(b[0]*a[1] - b[1]*a[0])
+    b_presses = (a[0]*p[1] - a[1]*p[0])/(a[0]*b[1] - a[1]*b[0])
+
+
+    if a_presses == int(a_presses) and b_presses == int(b_presses):
+        return cost_a * int(a_presses) + cost_b * int(b_presses)
+    else:
+        return 0
+
 
 def part_one(machines):
-    pass
+    return sum(button_press(machine) for machine in machines)
 
 
 def part_two(machines):
@@ -32,7 +43,7 @@ def part_two(machines):
 
 if __name__ == "__main__":
     file_path = "example.txt"
-    # file_path = "input.txt"
+    file_path = "input.txt"
 
     machines = load_machines(file_path)
 
